5612bfccff28c410cda50fe62fa0a4490807a5b4
unknown <kyleteeter12@gmail.com>
2019-05-25 11:38:27 -0500

    rounding to the nearest 100th

--- src/04_printing.py
+++ src/04_printing.py
@@ -11,7 +11,8 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
-print("x is %s, y is %s, z is %s" % (x, y, z))
+yRound = round(y, 2)
+print("x is %s, y is %s, z is %s" % (x, yRound, z))
 
 # Use the 'format' string method to print the same thing
 
